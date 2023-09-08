ecb6f4982679b630e46216f9f503688bef52ba61
Victor Raul <v1c70rotakmer@gmail.com>
2023-09-08 00:21:43 -0500

    Finish app wait deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(false)
     const handlerClick = () => {
-        console.log("click")
+        setOpenMenu(!openMenu)
     }
+    window.addEventListener("resize", () => {
+        if (window.innerWidth > 1024) {
+            setOpenMenu(false)
+        }
+    })
+    const stylesShowMenu = openMenu ? "duration-500 bg-white-400 max-lg:translate-x-[-178%]  max-lg:opacity-100 " : "max-lg:"
     return (
-        <header className="padding-x py-8 absolute z-10 w-full">
-            <nav className="flex justify-between items-center max-container">
+        <header className="padding-x py-8 fixed z-40 w-full ">
+            <nav className="flex justify-between items-center max-container relative">
                 <a href="/">
                     <img src={headerLogo} alt="logo" width={130} height={29} style={{padding:"10px"}}/>
                 </a>
-                <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                <ul className={`flex-1 flex justify-center items-center gap-16 max-lg:flex-col max-lg:h-screen max-lg:absolute max-lg:w-[50%] max-lg:translate-x-0 transition-transform  linear right-[-100%]  z-40 -top-8   ${stylesShowMenu}`}>
                     {
                         navLinks.map((link) => (
                             <li key={link.label}>
@@ -26,8 +34,8 @@ const Navbar = () => {
                         ))
                     }
                 </ul>
-                <div className="hidden max-lg:block cursor-pointer" onClick={handlerClick}> 
-                    <img src={hamburger} alt="Hamburguer icon" width={25} height={25} />
+                <div className="hidden max-lg:block cursor-pointer z-50" onClick={handlerClick}> 
+                    <img src={hamburger} alt="Hamburger icon" width={25} height={25} id="menu-btn" />
                 </div>
             </nav>
         </header>
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,51 @@
+import { copyrightSign } from "../assets/icons"
+import { footerLogo } from "../assets/images"
+import { footerLinks, socialMedia } from "../constants"
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="max-container ">
+            <div className="flex justify-between items-center gap-20 flex-wrap max-lg:flex-col ">
+                <div className="flex  flex-col items-start">
+                    <a href="/">
+                        <img src={footerLogo} alt="logo footer" width={150} height={46} />
+                    </a>
+                    <p className="mt-6 text-base leading-7 font-montserrat text-white sm:max-w-sm">Get shoes ready for the new term at your nearest Nike store. Find your perfect Size In Store. Get Rewards</p>
+                    <div className="flex items-center gap-5 mt-8">
+                        {
+                            socialMedia.map((item) => (
+                                <div key={item.alt} className="flex justify-center items-center w-12 h-12 bg-white rounded-full cursor-pointer hover:bg-white-400">
+                                    <img src={item.src} alt={item.alt} width={24} height={24} />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+                    {
+                        footerLinks.map((item) => (
+                            <div key={item.title} className="flex flex-col items-start">
+                                <h4 className="text-white font-montserrat text-2xl font-medium mb-6 leading-normal">{item.title}</h4>
+                                <ul>
+                                    {
+                                        item.links.map((link) => (
+                                            <li key={link.name} className="text-white text-base leading-7 font-montserrat cursor-pointer hover:text-white-400"><a href={link.link}>{link.name}</a></li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+                <div className="flex flex-1 justify-start items-center font-montserrat  cursor-pointer gap-3">
+                    <img src={copyrightSign} alt="copyright" width={20} height={20}  className="rounded-full m-0"/>
+                    <p>Copyright. All rights reserved</p>
+                </div>
+                <p className="font-montserrat cursor-pointer">Terms conditions</p>
+            </div>
+        </footer>
     )
 }
 
