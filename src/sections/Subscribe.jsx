2e84d6dadc3dcaaaa51207cb8b5c2b8f5f3f072f
Victor Raul <v1c70rotakmer@gmail.com>
2023-09-07 23:04:19 -0500

    Add section subscribe and reviews

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,20 @@
+import Button from "../components/Button"
 
 const Subscribe = () => {
     return (
-        <div>Subscribe</div>
+        <div className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
+            <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+                Sign up from 
+                <span className="text-coral-red"> Updates </span>
+                & Newsletters
+            </h3>
+            <div className="lg:max-w-[60%] w-full flex items-center max-sm:flex-col gap-5 p-3 sm:border sm:border-slate-gray rounded-full">
+                <input type="text" placeholder="subscribe@nike.com" className="input"/>
+                <div className="flex max-sm:justify-end items-center max-sm:w-full">
+                    <Button label="Subscribe" fullWidth />
+                </div>
+            </div>
+        </div>
     )
 }
 
